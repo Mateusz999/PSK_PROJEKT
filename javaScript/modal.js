fe520b748c91af1593cc3dc19fb958789765e0e6
Mateusz <mateuszbiernat070899@gmail.com>
2024-01-24 11:27:07 +0100

    add js modal function

---
+++ javaScript/modal.js
@@ -0,0 +1,35 @@
+const openModal = document.getElementById("openModal");
+const openModall = document.getElementById("openModall");
+const closeModal = document.getElementById("closeModal");
+const sendModal = document.getElementById("send");
+const infoModal = document.getElementById("informacja");
+
+openModal.addEventListener("click",()=>{
+    document.getElementById("modal").style.display="block";
+    document.getElementById("main").style.opacity="0.3";
+})
+openModall.addEventListener("click",()=>{
+    document.getElementById("modal").style.display="block";
+   document.getElementById("main").style.opacity="0.3";
+})
+closeModal.addEventListener("click",()=>{
+    document.getElementById("modal").style.display="none";
+    document.getElementById("main").style.opacity="1";
+})
+
+sendModal.addEventListener("click",()=>{
+    infoModal.innerHTML = "Wiadomość została wysłana na maila";
+    
+    setTimeout(()=>{
+        document.getElementById("modal").style.display="none";
+        document.getElementById("main").style.opacity="1";
+    infoModal.innerHTML = "";
+    },1000)
+})
+let modal = document.getElementById("main");
+window.onclick = (event)=> {
+if (event.target == modal) {
+document.getElementById("modal").style.display="none";
+document.getElementById("main").style.opacity="1";
+}
+}
